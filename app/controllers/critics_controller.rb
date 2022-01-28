194c910ed5200546b3dbd2c2d7adfc3a3156fc18
rgomeztinoco <gricardo2213@gmail.com>
2022-01-28 11:40:27 -0500

    routes and controllers

---
+++ app/controllers/critics_controller.rb
@@ -0,0 +1,18 @@
+class CriticsController < ApplicationController
+  def create
+    @critics = @criticable.critics.new(critics_params)
+  end
+
+  def destroy
+    @critics = @criticable.critics.new(critics_params)
+    @critics.destroy
+
+    redirect_to @criticable, status: :see_other
+  end
+
+  private
+
+  def critics_params
+    params.require(:critics).permit(:title, :body)
+  end
+end
