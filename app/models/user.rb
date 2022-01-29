0628c5467de9fa778efcaf6efcefb113da9163a8
Ximena Calderón <mxcalderons@gmail.com>
2022-01-29 17:23:24 -0500

    User and sessions views and functionality

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :username, presence: true, uniqueness: true, length: { maximum: 16 }
+
+  # Association
+  has_many :critics, dependent: :destroy
+end
