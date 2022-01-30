7b542afcf269f4a47b644ced60bd623c6a4902d3
Ximena Calderón <86806702+ximecalderon@users.noreply.github.com>
2022-01-30 12:43:31 -0500

    Mailer setup and Header CSS modifications

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings =
+    {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      user_name: ENV["GOOGLE_USERNAME"],
+      password: ENV["GOOGLE_APP_PWD"],
+      authentication: "plain",
+      enable_starttls_auto: true
+    }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
